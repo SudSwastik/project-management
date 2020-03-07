d74152a84adc21bbc3b085c0d1ccc5ee8b0f0c2e
SudarshanKumar <sudarshan4309@gmail.com>
2020-03-07 21:43:49 +0530

    Created an project entity

---
+++ project-management/src/main/java/com/sudarshan/sud/entities/Project.java
@@ -0,0 +1,48 @@
+package com.sudarshan.sud.entities;
+
+public class Project {
+
+	private long projectId;
+	private String name;
+	private String stage;
+	private String description;
+	
+	public Project() {
+		super();
+	}
+	
+	public Project(String name, String stage, String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public long getProjectId() {
+		return projectId;
+	}
+	
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStage() {
+		return stage;
+	}
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	
+}
