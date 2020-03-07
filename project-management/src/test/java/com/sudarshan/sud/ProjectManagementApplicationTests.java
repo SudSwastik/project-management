b14cf5de7d1d4c8a1c95f910e46584bd2e4494fd
SudarshanKumar <sudarshan4309@gmail.com>
2020-03-07 21:15:57 +0530

    Base Project

---
+++ project-management/src/test/java/com/sudarshan/sud/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudarshan.sud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
