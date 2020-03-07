a27147228f8a15f497ad7120f44eb6e7eb6f33ac
SudarshanKumar <sudarshan4309@gmail.com>
2020-03-08 01:05:10 +0530

    Added Model and View and Controller For Project Model

---
+++ project-management/src/main/java/com/sudarshan/sud/controllers/ProjectController.java
@@ -0,0 +1,32 @@
+package com.sudarshan.sud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sudarshan.sud.entities.Project;
+import com.sudarshan.sud.repositories.IProjectRepository;
+
+@Controller
+public class ProjectController {
+	
+	@Autowired
+	IProjectRepository projectRepository;
+	
+	@GetMapping("/new")
+	public String displayProjectForm (Model model){
+		 Project project = new Project();
+		 model.addAttribute("project", project);
+		return "new-project";
+	}
+	
+	@PostMapping("/save")
+	public String saveProjectForm (Project project, Model model){
+		projectRepository.save(project);
+		return "redirect:/new";
+	}
+
+}
---
+++ project-management/src/main/java/com/sudarshan/sud/repositories/IProjectRepository.java
@@ -0,0 +1,9 @@
+package com.sudarshan.sud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sudarshan.sud.entities.Project;
+
+public interface IProjectRepository extends CrudRepository<Project, Long>{
+
+}
